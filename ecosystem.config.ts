e3ea5bdfc1754fd1bd2cf98aa07dab9e1e71968d
Rotoris-Tech <admin@rotoris.com>
2025-11-07 15:19:49 +0530

    cleaned structure

---
+++ ecosystem.config.ts
@@ -0,0 +1,18 @@
+export default {
+  apps: [
+    {
+      name: 'prelaunch',
+      script: 'npm',
+      args: 'start',
+      cwd: '/home/ubuntu/Rotoris-headless-prelaunch',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000
+      }
+    }
+  ]
+};
